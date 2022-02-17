c0f65a5fe1c4e6cdfd836cd55f092c4d2ce428b5
kerev <backun-anatoly@yandex.ru>
2022-02-17 21:04:28 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import css from "./styles/style.css";
+import pug from "./index.pug";
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.pug"),
+      title: "Module_18",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+};
