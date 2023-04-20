3c2f37130b0633a29d13c8e770fc4c90d995163e
kerev <backun-anatoly@yandex.ru>
2023-04-20 22:26:35 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/index.scss'
+import $ from 'jquery'
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('.header').height($(window).height());
+//    })
+
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+console.log(mode + ' mode')
+
+module.exports = {
+    mode: mode,
+    output: {
+        assetModuleFilename: "asset/[hash][ext][query]",
+        clean: true
+    },
+
+    devServer: {
+        open: true,
+        hot: true,
+        port: 'auto',
+        static: {
+            directory: './src',
+            watch: true,
+        },
+    },
+    // devtool: 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HtmlWebpackPlugin(
+            {
+                template: './src/pages/index.pug',
+                filename: 'index.html'
+            }
+        ),
+        new HtmlWebpackPlugin(
+            {
+                template: './src/pages/about.pug',
+                filename: 'about.html'
+
+            }
+        ),
+        new HtmlWebpackPlugin(
+            {
+                template: './src/pages/contacts.pug',
+                filename: 'contacts.html'
+
+            }
+        )
+    ],
+
+    module: {
+
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },],],
+                            },
+                        },
+                    }, "sass-loader",],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                exclude: /(node_modules|bower_components)/,
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+
+
+}
